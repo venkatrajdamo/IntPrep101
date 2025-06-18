c7116921b9b0ac15fd5a2f728bdfa46cfdd6d571
Venkatraj Damodharan <venkatrajdamo@gmail.com>
2025-06-17 21:16:39 -0700

    Oracle Prep

--- src/Arrays/SubarraySumEqualsK.java
+++ src/Arrays/SubarraySumEqualsK.java
@@ -1,19 +1,19 @@
 package Arrays;
 
+import java.util.HashMap;
+
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int s = 0;
-        int e = 0;
-        int sum = 0;
+        HashMap<Integer, Integer> m = new HashMap<>();
         int count = 0;
-        for (e = 0; e < nums.length; e++) {
-            sum += nums[e];
-            while(sum > k){
-                sum -= nums[s];
-                s++;
+        int sum = 0;
+        m.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(m.containsKey(sum - k)){
+                count += m.get(sum - k);
             }
-            if (sum == k)
-                count++;
+            m.put(sum, m.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
